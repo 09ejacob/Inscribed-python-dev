0fcb679bd29f9ea1e5223b3d0e39414163136084
Elias Garseth Jacobsen <elias.garseth.jacobsen@gmail.com>
2024-06-30 18:19:12 +0200

    Moved fire a bit.

--- Code/game.py
+++ Code/game.py
@@ -97,7 +97,7 @@ class Game():
         background, bg_image = get_background("Blue.png", WIDTH, HEIGHT)
         block_size = 96
         self.player = Player(100, 100, 50, 50, player_id=self.network.id, skin="MaskDude" if self.network.id % 2 == 0 else "NinjaFrog")
-        fire = Fire(100, HEIGHT - block_size - 64, 16, 32)
+        fire = Fire(300, HEIGHT - block_size - 64, 16, 32)
         fire.on()
         floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
         objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire]
